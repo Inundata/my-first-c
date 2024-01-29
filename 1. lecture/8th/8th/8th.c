b6712e0e9c61c8032836b6f825b2d76fc931e6d3
Inundata <james.minho.lee@gmail.com>
2024-01-29 12:24:01 -0500

    # 20강 완료

---
+++ 1. lecture/8th/8th/8th.c
@@ -0,0 +1,538 @@
+// ============== 17강
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 10;
+//
+//	if (num1 == 10)
+//	{
+//		printf("10입니다.\n");
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//
+//	if (num1 == 10); // if조건문 끝에 세미콜론을 붙이면 안돼!
+//
+//	{
+//		printf("10입니다.\n");
+//
+//	}
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//
+//{
+//	int num1 = 10;
+//
+//	if (num1 == 10)
+//		printf("10입니다.\n");
+//
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//
+//	if (num1 == 10)
+//	{
+//		printf("if조건문\n");
+//		printf("10입니다.\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	float num1 = 0.1f;
+//	char c1 = 'a';
+//
+//	if (num1 == 0.1f)
+//		printf("0.1입니다.\n");
+//
+//	if (c1 == 'a')
+//		printf("a입니다.\n");
+//
+//	if (c1 == 97)
+//		printf("a입니다.\n");
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1;
+//	
+//	scanf("%d", &num1);
+//
+//	if (num1 == 10)
+//	{
+//		printf("10입니다.\n");
+//	}
+//
+//	if (num1 == 20)
+//	{
+//		printf("20입니다.\n");
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c1 = 'k';
+//
+//	if (c1 == 'k')
+//	{
+//		printf("k입니다.\n");
+//	}
+//	
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1;
+//	scanf("%d", &num1);
+//
+//	if (num1 < 18)
+//	{
+//		printf("청소년 관람불가");
+//
+//	}
+//
+//	return 0;
+//}
+
+// ============== 18강
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//
+//	if (num1 == 10)
+//	{
+//		printf("10입니다.\n");
+//	}
+//
+//	else
+//	{
+//		printf("10이 아닙니다.\n");
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 10;
+//
+//	if (num1 == 10)
+//	{
+//		printf("10입니다.\n");
+//	}
+//
+//	else;
+//	{
+//		printf("10이 아닙니다.\n");
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 20;
+//	
+//	if (num1 == 10)
+//		printf("10입니다.\n");
+//	else
+//		printf("10이 아닙니다.\n");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 20;
+//
+//	if (num1 == 10)
+//	{
+//		printf("if조건문\n");
+//		printf("10입니다.\n");
+//	}
+//
+//	else
+//	{
+//		printf("else조건문\n");
+//		printf("10이 아닙니다.\n");
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	if (2)
+//		printf("참\n");
+//	else
+//		printf("False\n");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//
+//	if (num1 = 10)
+//		printf("it is 10\n");
+//
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num = 10;
+//
+//	if (10 == num)
+//		printf("it is 10");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//
+//	if (10 == num1 && 20 == num2)
+//		printf("True\n");
+//	else
+//		printf("False\n");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	unsigned long long num1 = 0;
+//
+//	if (num1)
+//		printf("참\n");
+//	else
+//		printf("거짓\n");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int writtenTest = 78;
+//	int toeic = 870;
+//
+//	if (writtenTest >= 80 && toeic >= 850)
+//		printf("합격\n");
+//	else
+//		printf("불합격\n");
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c1;
+//
+//	scanf("%c", &c1);
+//
+//	if (c1 == 'a')
+//		printf("a입니다.\n");
+//	else
+//		printf("a가 아닙니다.\n");
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1, num2, num3, num4, mean;
+//
+//	scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
+//
+//	mean = (num1 + num2 + num3 + num4) / 4;
+//
+//	if (mean >= 85)
+//		printf("합격\n");
+//	else
+//		printf("불합격\n");
+//
+//	return 0;
+//}
+
+// ============== 19강
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 20;
+//
+//	if (num1 == 10)
+//		printf("it is 10.\n");
+//
+//	else if (num1 == 20)
+//		printf("it is 20.\n");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 30;
+//
+//	if (num1 == 10)
+//		printf("it is 10\n");
+//
+//	else if (num1 == 20)
+//		printf("it is 20\n");
+//
+//	else
+//		printf("it is neither 10 nor 20.\n");
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int balance = 10000;
+//	int age;
+//
+//	scanf("%d", &age);
+//
+//	if (age >= 7 && age <= 12)
+//		balance -= 450;
+//	else if (age >= 13 && age <= 18)
+//		balance -= 720;
+//	else if (age >= 19)
+//		balance -= 1200;
+//
+//	printf("%d\n", balance);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int num1 = 5;
+//    int num2 = 20;
+//    int num3 = 30;
+//
+//    if (num1 == 10)         // 조건식이 거짓
+//    {
+//        printf("10\n");
+//    }
+//    else if (num2 == 20)    // 조건식이 참
+//    {
+//        printf("20\n");     // 출력
+//    }
+//    else if (num3 == 30)    // 조건식을 검사하지 않고 건너뜀
+//    {
+//        printf("30");
+//    }
+//
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 10;
+//
+//	printf("%d\n", num1 == 10);
+//	printf("%d\n", num1 != 5);
+//
+//	printf("%d\n", num1 > 10);
+//	printf("%d\n", num1 < 10);
+//	
+//	printf("%d\n", num1 >= 10);
+//	printf("%d\n", num1 <= 10);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//	int num2;
+//
+//	if (num1)
+//		num2 = 100;
+//	else
+//		num2 = 200;
+//
+//	printf("%d\n", num2);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//	int num2;
+//
+//	num2 = num1 ? 100 : 200;
+//
+//	printf("%d\n", num2);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 10;
+//	int num2;
+//
+//	num2 = num1 == 10 ? 100 : 200;
+//
+//	printf("%d\n", num2);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 10;
+//
+//	if (num1 == 10)
+//		printf("it is 10.\n");
+//	if (num1 != 5)
+//		printf("it is not 5.\n");
+//	if (num1 > 10)
+//		printf("it is bigger than 10\n");
+//	if (num1 >= 10)
+//		printf("it is same or bigger than 10.\n");
+//	if (num1 <= 10)
+//		printf("it is same or smaller than 10.\n");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 5;
+//
+//	printf("%s\n", num1 == 10 ? "it is 10." : "it is not 10.");
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c1;
+//
+//	scanf("%c", &c1);
+//
+//	if (c1 != 'k')
+//		printf("참\n");
+//	else
+//		printf("거짓\n");
+//
+//	if (c1 > 'h')
+//		printf("참\n");
+//	else
+//		printf("거짓\n");
+//
+//	if (c1 <= 'o')
+//		printf("참\n");
+//	else
+//		printf("거짓\n");
+//
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+
+	scanf("%d", &num1);
+
+	printf("%d\n", num1 != 7 ? 1 : 2);
+
+	return 0;
+}
